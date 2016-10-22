f5e1a5478eb7365aea454087ab0280817d9e92af
Muhammad Ahsan <Muhammad Ahsan>
2016-10-23 00:02:54 +0500

    updated quiz application lab assignment

--- QuizApplication/app/src/main/java/com/example/muhammadahsan/quizapplication/MainActivity.java
+++ QuizApplication/app/src/main/java/com/example/muhammadahsan/quizapplication/MainActivity.java
@@ -14,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radioButtonAnswer1, radioButtonAnswer2, radioButtonAnswer3;
     RadioButton rbanswer1, rbanswer2, rbanswer3;
 
-    String r1, r2, r3, Result;
+    String r1, r2, r3, result, answer1, answer2, answer3, c1,c2, c3;
     int r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         radioGroup1 = (RadioGroup) findViewById(R.id.rg_1);
         radioGroup2 = (RadioGroup) findViewById(R.id.rg_2);
         radioGroup3 = (RadioGroup) findViewById(R.id.rg_3);
@@ -29,42 +30,63 @@ public class MainActivity extends AppCompatActivity {
         rbanswer2 = (RadioButton) findViewById(R.id.rb_ans_2);
         rbanswer3 = (RadioButton) findViewById(R.id.rb_ans_3);
 
-        Button button = (Button) findViewById(R.id.btn_submit);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int isSelected = radioGroup1.getCheckedRadioButtonId();
-                radioButtonAnswer1 = (RadioButton) findViewById(isSelected);
-
-
-
-
-
-            }
-        });
-
+        answer1 = rbanswer1.getText().toString();
+        answer2 = rbanswer2.getText().toString();
+        answer3 = rbanswer3.getText().toString();
     }
 
-    public void checkAnswers(View view) {
-        int isSelected = radioGroup1.getCheckedRadioButtonId();
-        radioButtonAnswer1 = (RadioButton) findViewById(isSelected);
-        r = isSelected;
-/*
-        if (radioButtonAnswer1.equals(-1)){
-            r1 = "Answer1 is not yet selected";
-        }else{
-            if (rbanswer1.getText().toString().equals(radioButtonAnswer1.getText().toString()) ){
-                r1 = "Answer1 is correct";
-            }else{
-                r1 = "Answer1 is not correcct";
-            }
+    public void chk_validation(View v){
+        if (radioGroup1.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(MainActivity.this, "Answer 1 is not yet selected", Toast.LENGTH_SHORT).show();
+        }
+        else if (radioGroup2.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(MainActivity.this, "Answer 2 is not yet selected", Toast.LENGTH_SHORT).show();
+        }
+        else if (radioGroup3.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(MainActivity.this, "Answer 3 is not yet selected", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            int isSelected1 = radioGroup1.getCheckedRadioButtonId();
+            radioButtonAnswer1 = (RadioButton) findViewById(isSelected1);
+            r1 = radioButtonAnswer1.getText().toString();
+
+            int isSelected2 = radioGroup2.getCheckedRadioButtonId();
+            radioButtonAnswer2 = (RadioButton) findViewById(isSelected2);
+            r2 = radioButtonAnswer2.getText().toString();
+
+            int isSelected3 = radioGroup3.getCheckedRadioButtonId();
+            radioButtonAnswer3 = (RadioButton) findViewById(isSelected3);
+            r3 = radioButtonAnswer3.getText().toString();
+
+            checkAnswers();
 
         }
-*/
+    }
 
-
-        //Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
-
+    public void checkAnswers(){
+        if (!answer1.equals(r1)){
+            c1 = "Answer 1 is not correct\n";
+        }else{
+            c1="";
+        }
+        if (!answer2.equals(r2)){
+            c2 = "Answer 2 is not correct\n";
+        }else{
+            c2="";
+        }
+        if (!answer3.equals(r3)){
+            c3 = "Answer 3 is not correct\n";
+        }else{
+            c3="";
+        }
+        result = c1+'\n'+c2+'\n'+c3;
+        //Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        if(result.length() <= 2){
+            result = "Congrats, All are correct";
+        }else{
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        }
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 }
