e00a8fdc4e60ba3cbe0842e0428641fb3863884b
Muhammad Ahsan <Muhammad Ahsan>
2016-12-18 23:43:56 +0500

    updated on 18 dec 2016

--- LMS/app/src/main/java/com/example/muhammadahsan/lms/MainActivity.java
+++ LMS/app/src/main/java/com/example/muhammadahsan/lms/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.muhammadahsan.reportcard;
+package com.example.muhammadahsan.lms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
---
+++ ReportCard/app/src/main/java/com/example/muhammadahsan/reportcard/ReportCard.java
@@ -0,0 +1,9 @@
+package com.example.muhammadahsan.reportcard;
+
+public class ReportCard {
+    private int studentID;
+    private String name;
+    private String Class;
+    private String Year;
+    03009868386
+}
